8ee9281b6ff5bfd4edfa503d7f95cf032507928d
hemanthkumarraya <68867420+hemanthkumarraya@users.noreply.github.com>
2025-12-01 11:50:39 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -34,7 +34,7 @@ st.subheader("Gloved vs. Bare Hand Monitoring")
 
 st.sidebar.header("Configuration")
 # Confidence slider for user control
-confidence = st.sidebar.slider("Confidence Threshold", 0.0, 1.0, 0.4)
+confidence = st.sidebar.slider("Confidence Threshold", 0.0, 1.0, 0.6)
 
 # --- File Uploader ---
 uploaded_file = st.file_uploader("Upload an image (.jpg, .jpeg, .png) for analysis:", 
